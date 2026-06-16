f9f1005b617c133fcec052519adb9a9d38ff7050
NazariyRomanyuk <naz.romanyuk@gmail.com>
2026-06-16 23:28:26 +0300

    Upload artifacts to GitHub Packages instead

--- AutomatizationLab6/build.gradle.kts
+++ AutomatizationLab6/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("java")
     id("checkstyle")
+    id("maven-publish")
 }
 
 group = "org.example"
@@ -24,4 +25,25 @@ tasks.test {
 
 checkstyle {
     toolVersion = "13.6.0"
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/NazariyRomanyuk/AutomatizationJava")
+            credentials {
+                username = System.getenv("USERNAME")
+                password = System.getenv("TOKEN")
+            }
+        }
+    }
+    publications {
+        register<MavenPublication>("jarPackage") {
+            from(components["java"])
+            groupId = group.toString()
+            artifactId = "jar-package"
+            version = version.toString()
+        }
+    }
 }
